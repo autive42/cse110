d8e0df3904f7a13f3864af443bbe44f9d5465899
autive42 <eheiner1764@gmail.com>
2022-01-29 18:57:52 -0700

    Updated meal.py

--- meal.py
+++ meal.py
@@ -1,12 +1,20 @@
-kids_meal = float(input("Enter the price of a child's meal: "))
-meal = float(input("Enter the price of an adult's meal: "))
+kids_meal = float(input("Enter the price of a child's meal: $"))
+meal = float(input("Enter the price of an adult's meal: $"))
 child = int(input("Enter the number of children: "))
 adult = int(input("Enter the number of adults: "))
 tax_rate = float(input("Enter the sales tax rate as a decimal: "))
 
 subtotal = kids_meal * child + meal * adult
-print(f"\nSubtotal: {subtotal}")
+print(f"\nSubtotal: ${subtotal}")
 
 tax = subtotal * tax_rate
 total = tax + subtotal
-print(f"Total: {total}")
+print(f"Total: ${total:.2f}")
+
+payment = float(input("\nEnter payment amount: $"))
+change = payment - total
+
+if payment < total:
+    print("Insufficient Funds")
+else:
+    print(f"Your change is ${change:.2f}")
