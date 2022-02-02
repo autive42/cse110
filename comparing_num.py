0b0e12e9cd85131d82701159d7b787645b40245e
autive42 <eheiner1764@gmail.com>
2022-02-02 10:30:05 -0700

    Added comparing assignment

---
+++ comparing_num.py
@@ -0,0 +1,14 @@
+int1 = int(input('Enter an integer: '))
+int2 = int(input('Enter an integer: '))
+
+print()
+if int1 == int2:
+    print('The numbers are equal')
+else:
+    print('The numbers are not equal')
+
+    if int1 > int2:
+        print('The first number is greater')
+    else:
+        print('The second number is greater')
+
---
+++ comparing_string.py
@@ -0,0 +1,7 @@
+my_animal = Snake
+usr_animal  = input('What is your favorite animal? ').capitalize()
+
+if my_animal == usr_animal:
+    print('\nNo way! We have the same favorite animal!')
+else:
+    print('\nWhat a dumb animal...')
