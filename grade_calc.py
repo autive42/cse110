f1f4e6a067f5bfa2893481eeb9709ea42abdc3f8
autive42 <eheiner1764@gmail.com>
2022-02-20 17:07:45 -0700

    edited gradecalc

--- grade_calc.py
+++ grade_calc.py
@@ -10,7 +10,18 @@ elif grade >= 70:
 else:
     letter = 'F'
 
-print(f"\nYou're grade is {letter}.")
+grade_last = grade % 10
+
+sign = ''
+if grade_last >= 7:
+    sign = '+'
+elif grade_last < 3:
+    sign = '-'
+
+if letter == 'F':
+    sign = ''
+
+print(f'\nYour final grade is {letter}{sign}.')
 
 if grade >= 70:
     print('Congratulations on passing!')
