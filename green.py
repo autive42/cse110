f044ffe91293a2d86a1a6cd4c9a0a07053e52389
autive42 <eheiner1764@gmail.com>
2022-03-14 17:11:22 -0600

    finished green.py

--- green.py
+++ green.py
@@ -3,18 +3,23 @@ from PIL import Image
 #make sure the images are in a folder called "images" in the script directory
 
 #load the image
-image_original = Image.open("images/beach.jpg")
+image_background = Image.open("images/beach.jpg")
+image_foreground = Image.open("images/penguin.jpg")
 
 #get the width and height and write to variables
-width, height = image_original.size
+width, height = image_foreground.size
 
 #get pixels
-pixels_original = image_original.load()
+pixels_background = image_background.load()
+pixels_foreground = image_foreground.load()
 
-#create a square in the corner
-for h in range(0, 64):
-    for w in range(0, 64):
-        pixels_original[w, h] = (235, 64, 52)
+#combine the two images with a greenscreen effect
+#absolute value for the green used is 44, 207, 64
+for h in range(height):
+    for w in range(width):
+        r, g, b, = pixels_foreground[w, h]
+        if r > 50 or g < 200 or b > 70:
+           pixels_background[w, h] = (r, g, b) 
 
-image_original.save("images/out.jpg")
-image_original.show()
+print("Wrote file to images/out.jpg")
+image_background.save("images/out.jpg")
