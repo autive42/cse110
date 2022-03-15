3763c9162e42c3e59a1ad5562298e92b335e3dfa
autive42 <eheiner1764@gmail.com>
2022-03-14 21:15:05 -0600

    added for loop practice

---
+++ for_loop_practice.py
@@ -0,0 +1,14 @@
+colors = ["red", "blue", "green", "yellow"]
+
+for color in colors:
+    print(color)
+
+print()
+
+for i in range(1, 9):
+    print(i)
+
+print()
+
+for l in range(2, 22, 2):
+    print(l)
