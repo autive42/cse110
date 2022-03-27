70ea3ab5069f0d751fedf1d66a5b260bb6e2da4c
autive42 <eheiner1764@gmail.com>
2022-03-27 01:32:26 -0600

    finished cart.py

--- cart.py
+++ cart.py
@@ -13,17 +13,44 @@ prices = []
 
 while True:
     action = int(float_input("\nPlease Select one:\n1. Add Item\n2. View Cart\n3. Remove Item\n4. Compute Total\n5. Quit\n(1-5): "))
+
+    #add item and price to list
     if action == 1:
-        #add item to list
-        cart.append(input("What item would you like to add? ").capitalize())
-        #prices.append(float_input("What is the price of the item? "))
+        cart.append(input("\nWhat item would you like to add? ").capitalize())
+        prices.append(float_input("What is the price of the item? "))
+        print(f'{cart[-1]} has been added for ${prices[-1]:.2f}')
+
     elif action == 2:
-        print("The items in the shopping cart are:")
-        for i in cart:
-            print(i)
-    elif action == 3 or action == 4:
-        print("\nThis action has not been configured yet")
+        print("\nThe items in the shopping cart are:")
+        for i in range(len(cart)):
+            print(f"{i + 1}. {cart[i]} for ${prices[i]:.2f}")
+
+    #calculate the total
+    elif action == 4: 
+        total = 0
+        for i in prices:
+            total += i
+        print(f"\nYour total is ${total:.2f}")
+
+    #remove an item
+    elif action == 3:
+        cartsize = len(cart)
+        print("\nSelect an item to remove:")
+        for i in range(cartsize):
+            print(f"{i + 1}. {cart[i]}")
+        selection = int(float_input("Please enter a number (0 to cancel): ")) - 1
+        if selection in range(cartsize):
+            removed_item = cart.pop(selection)
+            prices.pop(selection)
+            print(f"\n{removed_item} has been removed.")    
+        elif selection < 0:
+            print("\nNothing was removed.")
+        else:
+            print("\nPlease enter a number in the correct range.")
+
+    #quit
     elif action == 5:
         break
+
     else:
         print("\nPlease enter a number in the correct range.")
