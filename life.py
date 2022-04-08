7dee798377a6418de0ba33c6186cfc924837c0ba
autive42 <eheiner1764@gmail.com>
2022-04-08 16:44:15 -0600

    updated life.py

--- life.py
+++ life.py
@@ -1,20 +1,25 @@
 with open("life-expectancy.csv") as life_file:
     life_file.readline()
     largest = 0
-    smallest = 420
+    smallest = 0
+
+    life = []
+    code = []
+    year = []
+    entity = []
 
     for line in life_file:
         clean = line.strip()
         parts = clean.split(',')
-
-        entity = parts[0]
-        code = parts[1]
-        year = int(parts[2])
-        life = float(parts[3])
-
-        if life > largest:
-            largest = life
-        if life < smallest:
-            smallest = life
+        
+        entity.append(parts[0])
+        code.append(parts[1])
+        year.append(int(parts[2]))
+        life.append(float(parts[3]))
+        
+        if life[-1] > life[largest]:
+            largest = len(life) -1
+        if life[-1] < life[smallest]:
+            smallest = len(life) -1 
     
-    print('The largest life expectancy was {0}. The smallest life expectancy was {1}.'.format(largest, smallest))
+    print('The largest life expectancy was {0}. The smallest life expectancy was {1}.'.format(life[largest], life[smallest]))
